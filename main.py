c2d73196a2cce653ffb9a22044d9554d2d1c7c88
asukh <sukhavasiabilash@gmail.com>
2025-01-06 00:36:19 -0500

    minimal PDF files generated from EXCEL

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import glob
+import fpdf
+from pathlib import Path
 
 filepath = glob.glob("Data/*.xlsx")
 print(filepath)
 
 for file in filepath:
     df = pd.read_excel(file,sheet_name="Sheet 1")
-    print(df)
+    # print(df)
+
+    filename = Path(file).stem
+    invoice_nr = filename.split("-")
+    invoice_nr = invoice_nr[0]
+
+    pdf = fpdf.FPDF(orientation='P',unit='mm',format='A4')
+    pdf.add_page()
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50, h=8, txt=f'Invoice # {invoice_nr}',align="L",border=0)
 
 
+
+
+    pdf.output(f"Data/PDFs/{filename}.pdf")
 
