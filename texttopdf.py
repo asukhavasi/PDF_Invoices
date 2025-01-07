151813fd786b10878e31f3c52ca60dc8ba366a1d
asukh <sukhavasiabilash@gmail.com>
2025-01-06 23:07:04 -0500

    text to pdf code

--- texttopdf.py
+++ texttopdf.py
@@ -10,13 +10,21 @@ pdf = fpdf.FPDF(orientation="P",unit="mm",format="A4")
 
 # loop for number of files to add pdf pages
 for filename in filepaths:
-    # print(filename)
+
+    # extract only filename to copy as a header
     filename = (Path(filename).stem)
     new_filename = filename.title()
 
     pdf.add_page()
     pdf.set_font(family="Times",style="B",size=30)
-    pdf.cell(w=20,h=20,border=0,txt=new_filename)
+    pdf.cell(w=20,h=20,border=0,txt=new_filename,ln=1)
+
+    # Open text file
+    with open(filename,'r') as file:
+        text = file.read()
+
+    pdf.set_font(family="Times",size=10)
+    pdf.multi_cell(w=0,h=8,txt=text,align='J')
 
 # write output to pdf
 pdf.output(f"Data/PDFs/Animals.pdf")
