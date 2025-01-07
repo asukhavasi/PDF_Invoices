46785b3fcf8b2fec8b74413029be5749015f3141
asukh <sukhavasiabilash@gmail.com>
2025-01-06 21:25:31 -0500

    added data in pdf

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ filepath = glob.glob("Data/*.xlsx")
 print(filepath)
 
 for file in filepath:
-    df = pd.read_excel(file,sheet_name="Sheet 1")
+
     # print(df)
 
     filename = Path(file).stem
@@ -24,6 +24,27 @@ for file in filepath:
 
     pdf.set_font(family="Times",size=16,style="B")
     pdf.cell(w=50, h=8, txt=f'Date: {date}',align="L",border=0,ln=1)
+
+    df = pd.read_excel(file,sheet_name="Sheet 1")
+    df_header = list(df.columns)
+    columns = [item.replace("_"," ").title() for item in df_header]
+
+
+    # add header
+    pdf.set_font(family="Times", size=12,style="B")
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=50, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1,)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+    for  index,row in df.iterrows():
+        # Add Rows
+        pdf.set_font(family="Times",size=10)
+        pdf.cell(w=30,h=8,txt=str(row["product_id"]),border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40,h=8,txt=str(row["amount_purchased"]),border=1)
+        pdf.cell(w=30,h=8,txt=str(row["price_per_unit"]),border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1,ln=1)
 
 
 
